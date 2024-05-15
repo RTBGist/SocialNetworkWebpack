54d17615a87abefb9f8b526ee98583537613043d
DESKTOP-8IENF4O\User <yourdeveloper@mail.ru>
2024-05-16 00:29:44 +0700

    add webpack-dev-server, source-map, env variables

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -3,9 +3,10 @@ import {BuildOptions} from "./types/config";
 import {buildLoaders} from "./buildLoaders";
 import {buildResolves} from "./buildResolves";
 import {buildPlugins} from "./buildPlugins";
+import {buildDevServer} from "./buildDevServer";
 
 export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
-	const {paths, mode} = options
+	const {paths, mode, isDev} = options
 
 	return {
 		mode,
@@ -19,6 +20,8 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
 			rules: buildLoaders(),
 		},
 		resolve: buildResolves(),
-		plugins: buildPlugins(options)
+		plugins: buildPlugins(options),
+		devtool: isDev ? 'inline-source-map' : undefined,
+		devServer: isDev ? buildDevServer(options) : undefined
 	}
 }
