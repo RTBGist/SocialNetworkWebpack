cf143a79c7101e1c4fd3e3e9396a2b9d342d7921
DESKTOP-8IENF4O\User <yourdeveloper@mail.ru>
2024-06-28 09:23:21 +0700

    add svg loader, file loader in webpack loaders. add Button, ThemeSwitcher

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,7 @@
 import {classNames} from "shared/lib/className/classNames";
 import * as cls from './Navbar.module.scss';
 import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
+import {ThemeSwitcher} from "shared/ui/ThemeSwitcher";
 
 interface NavbarProps {
 	className?: string;
@@ -9,6 +10,7 @@ interface NavbarProps {
 export const Navbar = ({className}: NavbarProps) => {
 	return (
 			<div className={classNames(cls.navbar, {}, [className])}>
+				<ThemeSwitcher />
 				<div className={cls.links}>
 					<AppLink theme={AppLinkTheme.SECONDARY} to={'/'}>Главная</AppLink>
 					<AppLink theme={AppLinkTheme.SECONDARY}  to={'/about'}>О сайте</AppLink>
