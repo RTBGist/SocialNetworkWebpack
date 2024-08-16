0e135ef510f3d736bcf2b6f819fabbf7bef3b4fd
rinat42679 <mamedovrinat@crypton.studio>
2024-08-16 12:06:16 +0700

    add tests, storybook

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,16 +1,21 @@
 import { classNames } from 'shared/lib/className/classNames';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { useTranslation } from 'react-i18next';
 import * as cls from './Navbar.module.scss';
 
 interface NavbarProps {
   className?: string;
 }
 
-export const Navbar = ({ className }: NavbarProps) => (
-    <div className={classNames(cls.navbar, {}, [className])}>
-        <div className={cls.links}>
-            <AppLink theme={AppLinkTheme.SECONDARY} to="/">Главная</AppLink>
-            <AppLink theme={AppLinkTheme.SECONDARY} to="/about">О сайте</AppLink>
+export const Navbar = ({ className }: NavbarProps) => {
+    const { t } = useTranslation();
+
+    return (
+        <div className={classNames(cls.navbar, {}, [className])}>
+            <div className={cls.links}>
+                <AppLink theme={AppLinkTheme.SECONDARY} to="/">{t('Главная')}</AppLink>
+                <AppLink theme={AppLinkTheme.SECONDARY} to="/about">{t('О сайте')}</AppLink>
+            </div>
         </div>
-    </div>
-);
+    );
+};
