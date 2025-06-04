313dc35020cd688803d7f37f0707bad092abbf3e
Rinat <mamedovrinat@crypton.studio>
2025-06-04 19:18:29 +0700

    fix(pipeline): fix linter

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -18,7 +18,7 @@ export const Sidebar = (props: SidebarProps) => {
 
     return (
         <div data-testid="sidebar" className={classNames(cls.sidebar, { [cls.collapsed]: collapsed }, [className])}>
-            <button data-testid="siderbar-toggle" type="button" onClick={onToggle}>toggle</button>
+            {/* <button data-testid="siderbar-toggle" type="button" onClick={onToggle}>toggle</button> */}
             <div className={cls.switchers}>
                 <ThemeSwitcher />
                 <LangSwitcher className={cls.lang} />
