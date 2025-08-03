05dc98efb45a5bd445773e6fbf87375db04cd471
Rinat <mamedovrinat@crypton.studio>
2025-08-03 11:31:32 +0700

    feat(redux): add redux store

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { ErrorBoundary } from 'app/providers/ErrorBoundary';
 import { ThemeProvider } from 'app/providers/ThemeProvider';
 import './app/styles/index.scss';
 
+import { StoreProvider } from 'app/providers/StoreProvider';
 import App from './app/App';
 import 'shared/config/i18n/i18n';
 
@@ -11,11 +12,13 @@ const domNode = document.getElementById('root');
 const root = createRoot(domNode);
 
 root.render(
-    <BrowserRouter>
-        <ErrorBoundary>
-            <ThemeProvider>
-                <App />
-            </ThemeProvider>
-        </ErrorBoundary>
-    </BrowserRouter>,
+    <StoreProvider>
+        <BrowserRouter>
+            <ErrorBoundary>
+                <ThemeProvider>
+                    <App />
+                </ThemeProvider>
+            </ErrorBoundary>
+        </BrowserRouter>
+    </StoreProvider>,
 );
--- src/shared/config/tests/componentRender/componentRender.tsx
+++ src/shared/config/tests/componentRender/componentRender.tsx
@@ -2,22 +2,28 @@ import { ReactNode } from 'react';
 import { render } from '@testing-library/react';
 import { I18nextProvider } from 'react-i18next';
 import { MemoryRouter } from 'react-router-dom';
+import { StoreProvider } from 'app/providers/StoreProvider';
+import { StateSchema } from 'app/providers/StoreProvider/config/StateSchema';
 import i18nForTests from '../../i18n/i18nForTests';
 
 export interface componentRenderOptions {
-    route?: string
+    route?: string,
+    initialState?: StateSchema
 }
 
 export function componentRender(children: ReactNode, options: componentRenderOptions = {}) {
     const {
         route = '/',
+        initialState,
     } = options;
 
     return render(
-        <MemoryRouter initialEntries={[route]}>
-            <I18nextProvider i18n={i18nForTests}>
-                {children}
-            </I18nextProvider>
-        </MemoryRouter>,
+        <StoreProvider initialState={initialState}>
+            <MemoryRouter initialEntries={[route]}>
+                <I18nextProvider i18n={i18nForTests}>
+                    {children}
+                </I18nextProvider>
+            </MemoryRouter>
+        </StoreProvider>,
     );
 }
